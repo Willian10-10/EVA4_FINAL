8f4cc8322db9e0e3f1a022c46da01267b2375262
jonatan <jonacastillo22y@gmamil.com>
2024-12-12 15:12:09 -0300

    models y admin

--- reservasAPP/admin.py
+++ reservasAPP/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Reserva
 
-# Register your models here.
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'fecha', 'hora', 'cantidad_personas', 'estado')
+    search_fields = ('nombre', 'telefono', 'estado')
+    list_filter = ('estado', 'fecha')
--- reservasAPP/models.py
+++ reservasAPP/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Reserva(models.Model):
+    ESTADO_CHOICES = [
+        ('RESERVADO', 'Reservado'),
+        ('COMPLETADA', 'Completada'),
+        ('ANULADA', 'Anulada'),
+        ('NO_ASISTEN', 'No Asisten'),
+    ]
+    
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    cantidad_personas = models.PositiveSmallIntegerField()
+    estado = models.CharField(max_length=15, choices=ESTADO_CHOICES, default='RESERVADO')
+    observacion = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"Reserva de {self.nombre} para {self.cantidad_personas} personas el {self.fecha}"
