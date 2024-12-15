841f23e85a187af2b88f3ce9f2fadb46995496dc
jonatan <jonacastillo22y@gmamil.com>
2024-12-15 05:08:47 -0300

    navegacion entre paginas lista

--- reservasAPP/urls.py
+++ reservasAPP/urls.py
@@ -2,15 +2,19 @@ from django.urls import path
 from . import views, api_views
 
 urlpatterns = [
-    # Rutas para la interfaz web
-    path('', views.index, name='index'),  # Página principal
-    path('crear/', views.crear_reserva, name='reserva_create'),  # Ruta para crear una reserva
-    path('editar/<int:reserva_id>/', views.editar_reserva, name='editar_reserva'),  # Ruta para editar una reserva
-    path('reservas/', views.listar_reservas, name='listar_reservas'),  # Listar reservas
-    path('reservas/nueva/', views.crear_reserva, name='crear_reserva'),  # Ruta para crear una reserva
+    # Página principal
+    path('', views.index, name='index'),  # Redirige a landing_page
+    path('landing/', views.landing_page, name='landing_page'),  # Página de inicio
+
+    # Otras rutas
+    path('crear/', views.crear_reserva, name='reserva_create'),
+    path('editar/<int:reserva_id>/', views.editar_reserva, name='editar_reserva'),
+    path('reservas/', views.listar_reservas, name='listar_reservas'),
+    path('reservas/nueva/', views.crear_reserva, name='crear_reserva'),
     path('eliminar_reserva/<int:reserva_id>/', views.eliminar_reserva, name='eliminar_reserva'),
     
-    # Rutas de la API (para la administración de reservas)
-    path('api/reservas/', api_views.ReservaList.as_view(), name='api_reservas_list'),  # Listar todas las reservas o crear
-    path('api/reservas/<int:id>/', api_views.ReservaDetail.as_view(), name='api_reserva_detail'),  # Detalle, editar o eliminar una reserva
+    # Rutas de la API
+    path('api/reservas/', api_views.ReservaList.as_view(), name='api_reservas_list'),
+    path('api/reservas/<int:id>/', api_views.ReservaDetail.as_view(), name='api_reserva_detail'),
+    path('reservas/', views.listar_reservas, name='reservas'),
 ]
--- reservasAPP/views.py
+++ reservasAPP/views.py
@@ -9,6 +9,8 @@ from .serializers import ReservaSerializer
 
 def index(request):
     return render(request, 'index.html')
+def landing_page(request):
+    return render(request, 'landing_page.html')
 
 def listar_reservas(request):
     reservas = Reserva.objects.all()
