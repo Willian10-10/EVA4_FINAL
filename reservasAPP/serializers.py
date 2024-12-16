1fcf9d050a47668b8f85ab9a3ab2809b220e7ab7
jonatan <jonacastillo22y@gmamil.com>
2024-12-16 03:34:08 -0300

    esta todo terminado, solo falta mejorar el diseño de agregar reservas

--- reservasAPP/serializers.py
+++ reservasAPP/serializers.py
@@ -5,3 +5,8 @@ class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
         fields = ['id', 'nombre', 'telefono', 'fecha', 'hora', 'cantidad_personas', 'estado', 'observacion']
+
+    def validate_cantidad_personas(self, value):
+        if value < 1 or value > 15:
+            raise serializers.ValidationError("La cantidad de personas debe estar entre 1 y 15.")
+        return value
